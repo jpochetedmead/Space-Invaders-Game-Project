515aecb0cbe66f4d57f6335521111af4ee3c40bf
Nick Card <nickcard2000@gmail.com>
2019-12-04 12:59:36 -0500

    made the player move left and right. made player position relative to the canvas

--- script.js
+++ script.js
@@ -6,5 +6,28 @@
 
 
 //making the player move
-let player = documnent.body.getElementByClass("Player");
-let canvas = document.body.getElementById('canvas')
+let spaceship = document.getElementById('player');
+let canvas = document.getElementById('canvas');
+let position = 0;
+window.addEventListener("keydown", event => {
+    if (event.key == "ArrowLeft") {
+      console.log('left arrow was pressed');
+      position -= 10;
+        spaceship.style.left = position +'px'
+        //if (position >= )
+    }
+
+    
+    if (event.key == "ArrowRight"){
+        console.log('right arrow was pressed');
+        position += 10;
+        spaceship.style.left = position +'px'
+    }
+
+    if (event.key =="ArrowUp") {
+        console.log('fire button was pressed')
+    }
+    if (event.key == "r") {
+        console.log('refresh');
+    }
+  });
