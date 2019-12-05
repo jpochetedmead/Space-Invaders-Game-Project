59f0480dcb051af059835294003dc203452b2e43
Julio Pochet Edmead <jpe3841@gmail.com>
2019-12-05 05:49:52 -0500

    Try to work on the enemies movement...

--- script.js
+++ script.js
@@ -4,14 +4,14 @@
      * Space Invaders *
  **************************/
 /*
-1. As a Player, I want to press the arrow keys so I can move the space ship left and right.  (3)(In progress...)
-2. As a Player, I want to see the enemies, so I can aim at them and see their movements (3)
-3. As a Player, I want to press the up key, so I can see the bullet and shoot the enemies and see when enemies shoot at me (8)
+1. As a Player, I want to press the arrow keys so I can move the space ship left and right.  (3)(Done... can be improved.)
+2. As a Player, I want to see the enemies, so I can aim at them and see their movements (3)(In progress...)
+3. As a Player, I want to press the up key, so I can see the bullet and shoot the enemies and see when enemies shoot at me (8)(In progress...)
 4. As a Player, I want to see the enemies blow up, so I can tell that they are dead (5)
 5. As a Player, I want to see my lives and know when I die , so I can see when I’m going to die and when to restart (2)
 6. As a player I want to see the score, so I can see how well I’m doing (1)
 7. As a Player, I want to see an end screen , so I can tell that I beat the level (1)
-8. As a Player, I want to press a button , so I can restart the game (1)(Done.)
+8. As a Player, I want to press a button , so I can restart the game (1)(Done.. can be improved.)
 
 Controls:
 Left Arrow = Turn Left
@@ -82,7 +82,15 @@ window.addEventListener("keydown", event => {
     console.log(enemiesGrid);
     const node = document.createElement("div");
     const textnode = document.createTextNode(enemies[0]);
-    const canvas = document.querySelector('div.canvas');
+    const canvas = document.querySelector('div.enemies');
     node.appendChild(textnode);
     canvas.appendChild(node);
   }
+
+// Make enemies move by themselfs right, bottom, left, bottom, repeat.
+/* Trying to work on the enemies movement.... */
+const enemyDiv = document.getElementsByClassName('enemies');
+for (let count = 0; count < 100; count++) {
+  enemyDiv.style.top = count + "px";
+  enemyDiv.style.left = count + "px";
+}
