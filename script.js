a70e55c05e27c71c5cad3152e4f447acaf3b1c46
Julio Pochet Edmead <jpe3841@gmail.com>
2019-12-11 23:39:52 -0500

    Add semicolons, remove extra spaces, remove unnnecesary comments.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 1. As a Player, I want to press the arrow keys so I can move the space ship left and right.  (3)(Done... can be improved.)
 2. As a Player, I want to see the enemies, so I can aim at them and see their movements (3)(Done... can be improved.)
 3. As a Player, I want to press the up key, so I can see the bullet and shoot the enemies and see when enemies shoot at me (8)(In progress... almost done.)
-4. As a Player, I want to see the enemies blow up, so I can tell that they are dead (5)
+4. As a Player, I want to see the enemies blow up, so I can tell that they are dead (5)(Done...)
 5. As a Player, I want to see my lives and know when I die , so I can see when I’m going to die and when to restart (2)
 6. As a player I want to see the score, so I can see how well I’m doing (1)
 7. As a Player, I want to see an end screen , so I can tell that I beat the level (1)
@@ -31,107 +31,79 @@ let bulletAr = [];
 let enemiesGrid = [''];
 let test = document.querySelector(".test");
 
-
 function bulletEnemy() {
-
-    let bulletCol = document.querySelectorAll('.bullet')
-    let enemyCol = document.querySelectorAll('.enemies div')
-
+    let bulletCol = document.querySelectorAll('.bullet');
+    let enemyCol = document.querySelectorAll('.enemies div');
     for (let bullet of bulletCol) {
         for (let enemy of enemyCol){
             if ( enemy.textContent && collision(bullet, enemy)) {
                 bulletAr = bulletAr.slice(0, Number(bullet.dataset.index)).concat(bulletAr.slice(Number(bullet.dataset.index) + 1));
                 enemy.textContent = '';
-                
             }
-            
         }
-        
     }
 }
 
 function collision(node1, node2) {
     let test1 = node1.getBoundingClientRect();
-    
     let test2 = node2.getBoundingClientRect();
-    
+
     if (test1.x < test2.x + test2.width &&
         test1.x + test1.width > test2.x &&
         test1.y < test2.y + test2.height &&
         test1.y + test1.height > test2.y){
-
-            test.style.backgroundColor = 'red'
-        return true;
-        
+          test.style.backgroundColor = 'red';
+          return true;
         }
-        
 }
-
 collision(spaceship, test);
 
 function moveBulletUp() {
     for (let bullet of bulletAr) {
         bullet[1] += 5;
-       
         if (bullet[1] >= 650){
-            bulletAr.shift();   
+            bulletAr.shift();
        }
-        
     }
-   
-
 }
 
 function bulletStart(x) {
-    bulletAr.push([x, 24])
+    bulletAr.push([x, 24]);
 }
 
 function drawBullets() {
     removeBulletDivs();
     for (let index in bulletAr) {
-        
         bulletEl = document.createElement('div');
         bulletEl.className = 'bullet';
         bulletEl.dataset.index = index;
         bulletEl.style.left = bulletAr[index][0] + 18 + 'px';
         bulletEl.style.bottom = bulletAr[index][1] + 30 + 'px';
         canvas.appendChild(bulletEl);
-        
-        
     }
 }
 function removeBulletDivs() {
     for (let div of canvas.querySelectorAll('.bullet') ) {
         canvas.removeChild(div);
     }
-}
-
-
-
-
+  }
 
 //ANIMATE FUNCTION
 let lastTime = performance.now();
   function animate(now) {
+
     if (now - lastTime > 50){
-        
-    
-    lastTime = now
+    lastTime = now;
     moveBulletUp();
     bulletEnemy();
     drawBullets();
     requestAnimationFrame(animate);
     }
-    
-    
     else {
         requestAnimationFrame(animate);
     }
-   
   }
   requestAnimationFrame(animate);
-  
-  
 
 for (let counter = 0; playerGrid.length <= 1; counter ++) {
   playerGrid.push('');
@@ -143,7 +115,6 @@ for (let counter = 0; playerGrid.length <= 1; counter ++) {
 }
 
 // Making the player move
-
 window.addEventListener("keydown", event => {
     if (event.key == "ArrowLeft") {
       console.log('left arrow was pressed');
@@ -159,7 +130,7 @@ window.addEventListener("keydown", event => {
         console.log('right arrow was pressed');
         playerX += 15;
         spaceship.style.left = playerX +'px';
-        console.log(playerX)
+        console.log(playerX);
         if (playerX >= 515) {
             playerX -= 15;
             spaceship.style.left = playerX + 'px';
@@ -169,28 +140,25 @@ window.addEventListener("keydown", event => {
 // Making the player shoot
 // Up Arrow || SpaceBar to shoot.
     if (event.key === 'ArrowUp' || event.key === ' ') {
-        
+
         bulletStart(playerX);
         requestAnimationFrame(animate);
 
-        
-    
-    
     console.log('fire button was pressed');
-
 }
-// Restart the game
+
 // r key || R key to restart the game.
   if (event.key === 'r' || event.key === 'R') {
         console.log('refresh');
         window.location.reload();
+        playAgainButton.style.backgroundColor = 'green';
+        playAgainButton.style.color = 'white';
       }
-      collision(spaceship, test)
-      
+      collision(spaceship, test); //Why do we have this line here?
+
   });
 
 // Make enemies into an array
-  
   let enemies = ['👾'];
 
   for (let counter = 0; enemiesGrid.length <= 55; counter ++) {
@@ -203,7 +171,6 @@ window.addEventListener("keydown", event => {
   }
 
 // Make Enemies Move (right to left and viceversa while moving down at the same time)
-
 let enemyDiv = document.querySelector('.enemies');
 let countRightX = 0;
 let countDownY = 0;
@@ -217,23 +184,6 @@ let countDownY = 0;
       enemyDiv.style.top = angle + 10 + "px";
       enemyDiv.style.right = (Math.cos(angle) * 44) + "px";
     }
-
-
-
-/* I'll leave these comments here for now...
-//enemyDiv.style.top = (Math.sin(angle) * 20) + "px";
-//enemyDiv.style.left = (Math.cos(angle) * 44) + "px";
-//setTimeout(function() {countRightX += 1;}, 4500);
-//enemyDiv.style.top = (Math.sin(angle) + 20) + "px";
-//setTimeout(function() {enemyDiv.style.bottom = angle + 10 + "px";}, 3000);
-    if (countRightX === 1) {
-      console.log("YESSSS!")
-      enemyDiv.style.top = (Math.sin(angle) + 20) + "px";
-      enemyDiv.style.right = (Math.cos(angle) * 44) + "px";
-      //setTimeout(function() {countRightX += 1;}, 1500);
-      //enemyDiv.style.top = down + 20 + "px";
-      //enemyDiv.style.right = (Math.cos(angle) * 44) + "px";
-   } */
     requestAnimationFrame(newTime => animateEnemies(newTime, time));
   }
   requestAnimationFrame(animateEnemies);
@@ -245,7 +195,6 @@ const playAgainButton = document.querySelector('button.playAgainButton');
 playAgainButton.addEventListener("click", () => {
        window.location.reload();
      });
-
 
 //Game Controlers Div (Have them change colors whenever you do something with the spaceship)
 //Later we may add sounds.
